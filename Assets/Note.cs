2a65f2a8cf2022da24018d84308a59e709e788cc
Daehyun0504 <92385909+Daehyun0504@users.noreply.github.com>
2022-08-18 03:43:25 +0900

    first
    풀링못함

---
+++ Assets/Note.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+    public float noteSpeed = 400;
+
+    UnityEngine.UI.Image noteImage;
+
+    private void Start()
+    {
+        noteImage = GetComponent<UnityEngine.UI.Image>();
+    }
+
+    /*
+    void OnEnable() 
+    {
+        if (noteImage == null)
+            noteImage = GetComponent<UnityEngine.UI.Image>();
+
+        noteImage.enabled = true;
+    }
+    */
+
+    public void HideNote()
+    {
+        noteImage.enabled = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.right * noteSpeed * Time.deltaTime;
+    }
+
+
+}
